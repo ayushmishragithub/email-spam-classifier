3547d53bfa8394c118fdea0745db9dc810829068
ayushmishragithub <97837468+ayushmishragithub@users.noreply.github.com>
2022-04-26 21:44:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,16 +35,16 @@ def transform_text(text):
 tfidf = pickle.load(open('vectorizer.pkl','rb'))
 model = pickle.load(open('model.pkl','rb'))
 
-st.title("Email/SMS Spam Classifier")
+st.title("Email Spam Classifier")
 
 input_sms = st.text_area("Enter the message")
 
 if st.button('Predict'):
 
     # 1. preprocess
-    transformed_sms = transform_text(input_sms)
+    transformed_email = transform_text(input_sms)
     # 2. vectorize
-    vector_input = tfidf.transform([transformed_sms])
+    vector_input = tfidf.transform([transformed_email])
     # 3. predict
     result = model.predict(vector_input)[0]
     # 4. Display
